d6670b92727576d8293e1a8950316f8db9f8f0d6
Aaron Russell <aaron@gc4.co.uk>
2013-06-18 20:36:23 +0100

    Make the sweeper script slightly more robust.

--- script/stream.sh
+++ script/stream.sh
@@ -7,28 +7,28 @@ fifo="live.fifo.h264"
 set -x
 
 # Change to base directory
-cd $( dirname ${BASH_SOURCE[0]} )/..
+cd $( dirname "${BASH_SOURCE[0]}" )/..
 
 # Setup session
-mkdir $session
-rm -f $fifo live www/live
+mkdir "$session"
+rm -f "$fifo" live www/live
 ln -s "$PWD/$session" live
 ln -s "$PWD/$session" www/live
 
 
 # Pump raspivid into fifo
 # fifos seem to work more reliably than pipes
-mkfifo $fifo
+mkfifo "$fifo"
 raspivid \
   -w 1024 -h 576 -fps 25 -b 900000 \
-  -t 86400000 -hf -vf -o - | psips > $fifo &
+  -t 86400000 -hf -vf -o - | psips > "$fifo" &
 
 
 # Crank up ffmpeg
 # Spit segments into session path
 ffmpeg -y \
   -f h264 \
-  -i $fifo \
+  -i "$fifo" \
   -c:v copy \
   -map 0:0 \
   -f segment \
--- script/sweeper.sh
+++ script/sweeper.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
+# Add this to your crontab. Something like:
+# 0,31 * * * * /home/pi/pi-cam/script/sweeper.sh
+
 # Number of minutes to leave as buffer
-buffer=32
+buffer=31
 
 # Change to base directory
-cd $( dirname ${BASH_SOURCE[0]} )/..
+cd $( dirname "${BASH_SOURCE[0]}" )/..
 
 # Get a timestamp by:
   # list all ts files ordered by date
@@ -19,12 +22,16 @@ timestamp=$( ls -t live/ \
   # get the number of seconds the timestamp is from now
   # convert that into minutes
   # add on the buffer time
-minutes=$( expr $( date +%s ) - $timestamp \
-  | xargs -I {secs} expr {secs} / 60 \
-  | xargs -I {mins} expr {mins} + $buffer
-)
+if [[ -n $timestamp ]]; then
+  minutes=$( expr $( date +%s ) - "$timestamp" \
+    | xargs -I {secs} expr {secs} / 60 \
+    | xargs -I {mins} expr {mins} + "$buffer"
+  )
+fi
 
 # Sweep the out of date ts files by:
   # Find all sweepable files
   # and purge em!
-find live/ -type f -name '*.ts' -mmin +$minutes -print0 | xargs -r -0 rm
+if [[ -n $minutes ]]; then
+  find live/ -type f -name '*.ts' -mmin "+$minutes" -print0 | xargs -r -0 rm
+fi
