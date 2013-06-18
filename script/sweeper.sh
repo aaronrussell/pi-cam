1d3bb6b3e3e88e5f0a60064af135771389435072
Aaron Russell <aaron@gc4.co.uk>
2013-06-18 20:43:35 +0100

    Make sweeper file handle when no live dir exists.

--- script/sweeper.sh
+++ script/sweeper.sh
@@ -9,29 +9,32 @@ buffer=31
 # Change to base directory
 cd $( dirname "${BASH_SOURCE[0]}" )/..
 
-# Get a timestamp by:
+# If the live directory exists,
+# get the timestamp of the most recent ts file
+if [[ -d live/ ]]; then
   # list all ts files ordered by date
   # get the most recent ts file
   # get the timestamp of that file
-timestamp=$( ls -t live/ \
-  | head -1 \
-  | xargs -I {file} stat -c %Y live/{file}
-)
+  timestamp=$( ls -t live/ \
+    | head -1 \
+    | xargs -I {file} stat -c %Y live/{file}
+  )
+fi
 
-# Get a number of minutes by:
+# If the timestamp has been set,
+# convert it to a number of minutes to find files from
+if [[ -n $timestamp ]]; then
   # get the number of seconds the timestamp is from now
   # convert that into minutes
   # add on the buffer time
-if [[ -n $timestamp ]]; then
   minutes=$( expr $( date +%s ) - "$timestamp" \
     | xargs -I {secs} expr {secs} / 60 \
     | xargs -I {mins} expr {mins} + "$buffer"
   )
 fi
 
-# Sweep the out of date ts files by:
-  # Find all sweepable files
-  # and purge em!
+# If the minutes integer has been set
+# find all sweepable files, and sweep em away!
 if [[ -n $minutes ]]; then
   find live/ -type f -name '*.ts' -mmin "+$minutes" -print0 | xargs -r -0 rm
 fi
