9b4c29995b1f6b987d3cec5b417fc6683da9b373
Aaron Russell <aaron@gc4.co.uk>
2013-06-18 20:16:21 +0100

    Break up the sweeper script into something a little more readable.

--- script/sweeper.sh
+++ script/sweeper.sh
@@ -3,24 +3,28 @@
 # Number of minutes to leave as buffer
 buffer=32
 
-set -x
-
 # Change to base directory
 cd $( dirname ${BASH_SOURCE[0]} )/..
 
-# list all ts files ordered by date
+# Get a timestamp by:
+  # list all ts files ordered by date
   # get the most recent ts file
   # get the timestamp of that file
-  # get the num of seconds from now file was created
+timestamp=$( ls -t live/ \
+  | head -1 \
+  | xargs -I {file} stat -c %Y live/{file}
+)
+
+# Get a number of minutes by:
+  # get the number of seconds the timestamp is from now
   # convert that into minutes
   # add on the buffer time
-  # find all purgable files
-  # and purge em!
-ls -t live/ \
-  | head -1 \
-  | xargs -I {file} stat -c %Y live/{file} \
-  | xargs -I {ts} expr $( date +%s ) - {ts} \
+minutes=$( expr $( date +%s ) - $timestamp \
   | xargs -I {secs} expr {secs} / 60 \
-  | xargs -I {mins} expr {mins} + $buffer \
-  | xargs -I {mins} find live/ -type f -name '*.ts' -mmin +{mins} -print0 \
-  | xargs -r -o rm
+  | xargs -I {mins} expr {mins} + $buffer
+)
+
+# Sweep the out of date ts files by:
+  # Find all sweepable files
+  # and purge em!
+find live/ -type f -name '*.ts' -mmin +$minutes -print0 | xargs -r -0 rm
